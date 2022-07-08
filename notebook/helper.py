a42f5ef4faebbf3740fac95954ebdaad63737b4e
Aete <hsg8234@naver.com>
2022-07-08 20:22:28 +0900

    🚧 [P] Economic Density of high-level regions
    1. [P, Processing]  Get the data from API.ipynb:
    - write codes to get the Economic Density of high-level regions.
    
    2. helper.py:
    - dict_hl_name: dictionary to match different high level region names
    -  map_hl: function to fix the high level region names with dict_hl_name

--- notebook/helper.py
+++ notebook/helper.py
@@ -37,3 +37,38 @@ def request_data_kosis(url, target_period, index = ['C1', 'C1_NM'], columns = 'I
     df_given_data.columns.name = ''
 
     return df_given_data
+
+dict_hl_name = {
+    '서울특별시' : ['서울', '서울시', '서울특별시'],
+    '부산광역시' : ['부산', '부산시', '부산광역시'],
+    '대구광역시' : ['대구', '대구시', '대구광역시'],
+    '인천광역시' : ['인천', '인천시', '인천광역시'],
+    '광주광역시' : ['광주', '광주시', '광주광역시'],
+    '대전광역시' : ['대전', '대전시', '대전광역시'],
+    '울산광역시' : ['울산', '울산시', '울산광역시'],
+    '세종특별자치시': ['세종', '세종시', '세종특별자치시'],
+    '경기도' : ['경기도', '경기'],	
+    '강원도' : ['강원도', '강원'],
+    '충청북도' : ['충북', '충청북도'],
+    '충청남도' : ['충남', '충청남도'],	
+    '전라북도' : ['전북', '전라북도'],
+    '전라남도' : ['전남', '전라남도'],
+    '경상북도' : ['경북', '경상북도'],	
+    '경상남도' : ['경남', '경상남도'],	
+    '제주특별자치도' : ['제주']
+}
+
+def map_hl(city, dictionary):
+    '''
+    This function is for unifying different high_level region names
+    -----
+    input:
+        city(string): high level region name
+        dictionary(dict): dict_hl_name
+    -----
+    output:
+        k(string): unified high level region name
+    '''
+    for k,v in dictionary.items():
+        if city in v:
+            return k
